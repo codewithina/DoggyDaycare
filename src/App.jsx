39154fca44e17a29636141737366bd6d90437053
Ina Burström <inaburstrom@hotmail.com>
2024-09-18 17:35:08 +0200

    Changed router from app to main.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import WelcomePage from './components/WelcomePage';
 import DogCatalog from './components/DogCatalog';
 import DogDetails from './components/DogDetails';
@@ -9,13 +9,11 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <Router>
       <Routes>
         <Route path="/" element={<WelcomePage />} />
         <Route path="/catalog" element={<DogCatalog />} />
         <Route path="/dog/:id" element={<DogDetails />} />
       </Routes>
-    </Router>
   )
 }
 
