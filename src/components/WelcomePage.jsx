18d54709fb9348af96b42e9a2a873aef9069ecb8
Ina Burström <inaburstrom@hotmail.com>
2024-09-20 15:29:26 +0200

    Css files added and welcomepage updated with link to catalog.

--- src/components/WelcomePage.jsx
+++ src/components/WelcomePage.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import './WelcomePageStyle.css';
 
 const WelcomePage = () => {
-  return <h1>Welcome to Doggy Daycare</h1>;
-};
+  return (
+    <div className="welcome-page">
+      <h1>Välkommen till Doggy Daycare</h1>
+      <p>Vi tar hand om dina älskade hundar!</p>
+      <a href="/#/catalog" className="btn">Se våra hundar</a>
+    </div>
+  );
+}
 
 export default WelcomePage
